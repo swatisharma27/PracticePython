3c4f332b8fe75df81217492cb990ce614da7197f
swatisharma27 <41550990+swatisharma27@users.noreply.github.com>
2018-12-10 22:55:12 -0600

    Update ques_1.py

--- Assignment_36ques/ques_1.py
+++ Assignment_36ques/ques_1.py
@@ -1,9 +1,9 @@
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
-diff_of_age = str(100 - age)
-print("Hi " + name + "! " + "You are going to complete a century in " + diff_of_age + " years.")
+diff_of_age = str((2018 - age)+100)
+print("Hi " + name + "! " + "You are going to complete a century in the year: " + diff_of_age )
 
 number = int(input("Enter a number: "))
 
 for itr in range(number):
-    print("Hi " + name + "! " + "You are going to complete a century in " + diff_of_age + " years" + "\n")
+    print("Hi " + name + "! " + "You are going to complete a century in the year: " + diff_of_age + "\n")
